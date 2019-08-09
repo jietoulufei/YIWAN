7a44c74bf531eaf688b817340b3c622901e24432
jietoulufei <914190601@qq.com>
2019-08-09 17:40:28 +0800

    #总览chart图初版#

--- src/app/index-page/coding/all-view/all-view.component.ts
+++ src/app/index-page/coding/all-view/all-view.component.ts
@@ -31,83 +31,213 @@ export class AllViewComponent implements OnInit {
       }, 200)
     });
 
+    this.chartInit();
+  }
+
+  /**
+   * chart图数据加载 
+   */
+  chartInit() {
+
+    var builderJson = {
+      "all": 10887,
+      "charts": {
+        "map": 3237,
+        "lines": 2164,
+        "bar": 7561,
+        "line": 7778,
+        "pie": 7355,
+        "scatter": 2405,
+        "candlestick": 1842,
+        "radar": 2090,
+        "heatmap": 1762,
+        "treemap": 1593,
+        "graph": 2060,
+        "boxplot": 1537,
+        "parallel": 1908,
+        "gauge": 2107,
+        "funnel": 1692,
+        "sankey": 1568
+      },
+      "ie": 9743
+    };
+
+    var downloadJson = {
+      "echarts.min.js": 17365,
+      "echarts.simple.min.js": 4079,
+      "echarts.common.min.js": 6929,
+      "echarts.js": 14890
+    };
+
+    /**
+     * chart背景水印
+     */
+    const waterMarkText = 'TenThousand';
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = canvas.height = 100;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.globalAlpha = 0.08;
+    ctx.font = '20px Microsoft Yahei';
+    ctx.translate(50, 50);
+    ctx.rotate(-Math.PI / 4);
+    ctx.fillText(waterMarkText, 0, 0);
+
     this.chartOption = {
-      title: {
-        text: '堆叠区域图'
+      backgroundColor: {
+        type: 'pattern',
+        image: canvas,
+        repeat: 'repeat'
       },
-      tooltip: {
-        trigger: 'axis'
-      },
+      tooltip: {},
       legend: {
-        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        orient: 'vertical',
+        x: 'right',
+        y: 'bottom',
+        data: ['直接访问', '邮件营销']
       },
-      toolbox: {
+      toolbox: {  //保存下载
         feature: {
           saveAsImage: {}
         }
       },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
+      title: [{
+        text: '在线构建',
+        subtext: '总计 ' + builderJson.all,
+        x: '25%',
+        textAlign: 'center'
+      }, {
+        text: '各版本下载',
+        subtext: '总计 ' + Object.keys(downloadJson).reduce(function (all, key) {
+          return all + downloadJson[key];
+        }, 0),
+        x: '75%',
+        textAlign: 'center'
+      }],
+      grid: [{
+        top: 50,
+        width: '50%',
+        bottom: '0%',
+        left: 10,
         containLabel: true
-      },
-      xAxis: [
-        {
-          type: 'category',
-          boundaryGap: false,
-          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      }],
+      xAxis: [{
+        type: 'value',
+        max: builderJson.all,
+        splitLine: {
+          show: false
         }
-      ],
-      yAxis: [
-        {
-          type: 'value'
+      }],
+      yAxis: [{
+        type: 'category',
+        data: Object.keys(builderJson.charts),
+        axisLabel: {
+          interval: 0,
+          rotate: 30
+        },
+        splitLine: {
+          show: false
         }
-      ],
-      series: [
-        {
-          name: '邮件营销',
-          type: 'line',
-          stack: '总量',
-          areaStyle: { normal: {} },
-          data: [120, 132, 101, 134, 90, 230, 210]
+      }],
+      series: [{
+        type: 'bar',
+        stack: 'chart',
+        z: 3,
+        label: {
+          normal: {
+            position: 'right',
+            show: true
+          }
         },
-        {
-          name: '联盟广告',
-          type: 'line',
-          stack: '总量',
-          areaStyle: { normal: {} },
-          data: [220, 182, 191, 234, 290, 330, 310]
+        itemStyle: {  //柱状图样式
+          normal: {
+            color: '#1890FF'
+          }
         },
-        {
-          name: '视频广告',
-          type: 'line',
-          stack: '总量',
-          areaStyle: { normal: {} },
-          data: [150, 232, 201, 154, 190, 330, 410]
+        data: Object.keys(builderJson.charts).map(function (key) {
+          return builderJson.charts[key];
+        })
+      }, {
+        type: 'bar',
+        stack: 'chart',
+        silent: true,
+        itemStyle: { //柱状图背景样式
+          normal: {
+            color: '#eee'
+          }
         },
-        {
-          name: '直接访问',
-          type: 'line',
-          stack: '总量',
-          areaStyle: { normal: {} },
-          data: [320, 332, 301, 334, 390, 330, 320]
-        },
-        {
-          name: '搜索引擎',
-          type: 'line',
-          stack: '总量',
-          label: {
-            normal: {
-              show: true,
-              position: 'top'
+        data: Object.keys(builderJson.charts).map(function (key) {
+          return builderJson.all - builderJson.charts[key];
+        })
+      }, {
+        type: 'pie', //饼图
+        radius: [0, '30%'],
+        center: ['75%', '25%'],
+        itemStyle: {
+          normal: {
+            //好，这里就是重头戏了，定义一个list，然后根据所以取得不同的值，这样就实现了，
+            color: function (params) {
+              // build a color map as your need.
+              var colorList = [
+                '#3CB2EF', '#67E0E3', '#FFDB5C', '#FFAE8B', '#96BFFF',
+              ];
+              return colorList[params.dataIndex]
             }
+          }
+        },
+        data: Object.keys(downloadJson).map(function (key) {
+          return {
+            name: key.replace('.js', ''),
+            value: downloadJson[key]
+          }
+        })
+      }, {
+        name: '访问来源',
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        type: 'pie',
+        radius: ['20%', '30%'],
+        center: ['75%', '75%'],
+        avoidLabelOverlap: false,
+        label: {
+          normal: {
+            show: false,
+            position: 'center'
           },
-          areaStyle: { normal: {} },
-          data: [820, 932, 901, 934, 1290, 1330, 1320]
-        }
-      ]
+          emphasis: {
+            show: true,
+            textStyle: {
+              fontSize: '30',
+              fontWeight: 'bold'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            show: false
+          }
+        },
+        itemStyle: {
+          normal: {
+            //好，这里就是重头戏了，定义一个list，然后根据所以取得不同的值，这样就实现了，
+            color: function (params) {
+              // build a color map as your need.
+              var colorList = [
+                '#3CB2EF', '#FFED65'
+              ];
+              return colorList[params.dataIndex]
+            }
+          }
+        },
+        data: [
+          { value: 335, name: '直接访问' },
+          { value: 310, name: '邮件营销' },
+        ]
+      }]
     }
   }
- 
 }
