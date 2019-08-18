f3f44f9661ceaa0d4d3378ac97f9b633d6b23f60
jietoulufei <914190601@qq.com>
2019-08-18 21:11:19 +0800

    #路由监听调整css#

--- src/app/index-page/index-page.component.ts
+++ src/app/index-page/index-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MethodsService } from '../share/methods.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-index-page',
@@ -11,17 +13,41 @@ export class IndexPageComponent implements OnInit {
    * 侧边栏收缩状态 false=展开，true=收缩
    */
   isCollapsed = false;
-  constructor(private sideBarMs$:MethodsService) { }
+  /**
+   * 编辑页样式触发条件
+   */
+  isEditClick: boolean = false;
+  /**
+   * 其他常规页触发条件
+   */
+  isNormalClick:boolean = true;
+  constructor(private sideBarMs$: MethodsService, private router: Router) { }
 
   ngOnInit() {
+    /**
+     * 监听路由 --编辑页样式需要单独修改
+     */
+    this.router.events.
+      pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        console.log(event.url);
+        if (event.url.indexOf('editCoding') > -1) {
+          this.isEditClick = true;
+          this.isNormalClick = false;
+        }else{
+          this.isEditClick = false;
+          this.isNormalClick = true;
+        }
+      });
+      
   }
 
   /**
    * 收缩框点击
    */
-  changeSideBar(){
+  changeSideBar() {
     this.isCollapsed = !this.isCollapsed;
-    console.log("this.isCollapse",this.isCollapsed);
+    console.log("this.isCollapse", this.isCollapsed);
     this.sideBarMs$.sendSideEvent(this.isCollapsed);
   }
 
