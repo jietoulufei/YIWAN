8b539bf2f96102aa0e709332c881bfe00f7a480d
jietoulufei <914190601@qq.com>
2019-09-25 17:51:55 +0800

    #js-xlsx库导入完善excel导出功能#

--- src/app/index-page/coding/edit-coding/edit-coding.component.ts
+++ src/app/index-page/coding/edit-coding/edit-coding.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MethodsService } from 'src/app/share/methods.service';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-edit-coding',
   templateUrl: './edit-coding.component.html',
   styleUrls: ['./edit-coding.component.css']
 })
 export class EditCodingComponent implements OnInit {
+  @ViewChild('basicTable', { static: true }) greetDiv: ElementRef;
   /**
    * 编辑 日期单元格 样式
    */
@@ -234,36 +235,66 @@ export class EditCodingComponent implements OnInit {
   }
 
   /**
-   * 下载Excel
+   * 下载Excel ===================================================================start
    */
   downExcel() {
-    //要导出的json数据 格式
-    // const jsonData = [{
-    //   name: '路人甲',
-    //   phone: '123456789',
-    //   email: '000@123456.com'
-    // }];
+    console.log("this.greetDiv", this.greetDiv["nzData"]);
+    //var table1 = document.querySelector("#table1");
 
-    //列标题，逗号隔开，每一个逗号就是隔开一个单元格
-    let str = `名称,类别,起始日,终止日,总时长\n`;
-    //增加\t为了不让表格显示科学计数法或者其他格式
-    for (let i = 0; i < this.excelData.length; i++) {
-      for (let item in this.excelData[i]) {
-        str += `${this.excelData[i][item] + '\t'},`;
-      }
-      str += '\n';
+    let aoa = this.greetDiv["nzData"].map(item => {
+      return [item.name, item.type, item.startTime, item.endTime, item.time]
+    })
+    aoa = [["名称", "类别", "起始日", "终止日", "总时长"], ...aoa];
+    //var sheet = XLSX.utils.table_to_sheet(table1);//将一个table对象转换成一个sheet对象
+    var sheet = XLSX.utils.aoa_to_sheet(aoa);
+    this.openDownloadDialog(this.sheet2blob(sheet), '下载.xlsx');
+  };
+
+  // 将一个sheet转成最终的excel文件的blob对象，然后利用URL.createObjectURL下载
+  sheet2blob(sheet, sheetName?) {
+    sheetName = sheetName || 'sheet1';
+    var workbook = {
+      SheetNames: [sheetName],
+      Sheets: {}
+    };
+    workbook.Sheets[sheetName] = sheet; // 生成excel的配置项
+
+    var wopts = {
+      bookType: "xlsx", // 要生成的文件类型
+      bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+      type: "binary"
+    };
+    //var wbout = XLSX.write(workbook, wopts);
+    var wbout = XLSX.write(workbook, { bookType: "xlsx", bookSST: false, type: "binary" });
+    var blob = new Blob([s2ab(wbout)], {
+      type: "application/octet-stream"
+    }); // 字符串转ArrayBuffer
+    function s2ab(s) {
+      var buf = new ArrayBuffer(s.length);
+      var view = new Uint8Array(buf);
+      for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+      return buf;
     }
-
-    //encodeURIComponent解决中文乱码
-    let uri = 'data:text/csv;charset=utf-8,\ufeff' + encodeURIComponent(str);
-    //通过创建a标签实现
-    let link = document.createElement("a");
-    link.href = uri;
-    //对下载的文件命名
-    link.download = "json数据表.csv";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    return blob;
   }
 
+  openDownloadDialog(url, saveName) {
+    if (typeof url == 'object' && url instanceof Blob) {
+      url = URL.createObjectURL(url); // 创建blob地址
+    }
+    var aLink = document.createElement('a');
+    aLink.href = url;
+    aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+    var event;
+    // if (window.MouseEvent) event = new MouseEvent('click');
+    // else {
+    event = document.createEvent('MouseEvents');
+    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    //}
+    aLink.dispatchEvent(event);
+  };
+
+  /**
+   * 下载Excel ===================================================================end
+   */
 }
